6bdc4ad5816954471f509965ccb4de15d9dc6ad4
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-06-25 04:13:58 +0200

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Manyuchi, Carrington C on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
